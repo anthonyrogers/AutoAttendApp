47ab6287489ea99deba28036ab21ab652b3f5feb
Anthony <ar.anthonyrogers@gmail.com>
2020-04-07 18:21:32 -0400

    added google play services needed to access the nearby messages and added links to the account type page

--- app/src/main/java/com/example/autoattendapp/AccountType.java
+++ app/src/main/java/com/example/autoattendapp/AccountType.java
@@ -2,13 +2,36 @@ package com.example.autoattendapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class AccountType extends AppCompatActivity {
+
+    ImageView student, teacher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_type);
+
+        student = findViewById(R.id.studentAccPic);
+        teacher = findViewById(R.id.teacherAccPic);
+
+        student.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent createAcc = new Intent(AccountType.this, CreateAccountActivity.class);
+                startActivity(createAcc);
+            }
+        });
+
+        teacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
