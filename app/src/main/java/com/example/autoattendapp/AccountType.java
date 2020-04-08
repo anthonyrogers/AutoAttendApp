d1b27ead277c54b7abf55e0f78c82c17e168ba9c
Anthony <ar.anthonyrogers@gmail.com>
2020-04-07 21:31:04 -0400

    Added 3rd party SDK and Created proper methods to search for beacon in proximity. Added permissions for bluetooth as well.

--- app/src/main/java/com/example/autoattendapp/AccountType.java
+++ app/src/main/java/com/example/autoattendapp/AccountType.java
@@ -4,17 +4,33 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
-public class AccountType extends AppCompatActivity {
+import com.google.android.gms.nearby.Nearby;
+import com.google.android.gms.nearby.messages.Message;
+import com.google.android.gms.nearby.messages.MessageListener;
+
+import org.altbeacon.beacon.Beacon;
+import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
+
+public class AccountType extends AppCompatActivity  {
 
     ImageView student, teacher;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_type);
+
+
 
         student = findViewById(R.id.studentAccPic);
         teacher = findViewById(R.id.teacherAccPic);
